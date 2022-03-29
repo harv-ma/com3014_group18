59ef43def199e0ea7784dcbd34ad992714d42e32
Patrick Oramah <patrickoramah98@gmail.com>
2022-03-29 21:52:03 +0100

    created API Gateway project

---
+++ backend/APIGateway/src/test/java/com/group18/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group18.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
