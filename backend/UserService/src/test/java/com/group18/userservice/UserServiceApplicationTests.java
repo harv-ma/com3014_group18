78b36d393cd859e4ac4f1e48d0174f8e47f73d60
Patrick Oramah <patrickoramah98@gmail.com>
2022-03-30 08:21:26 +0100

    Created user service

---
+++ backend/UserService/src/test/java/com/group18/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group18.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
