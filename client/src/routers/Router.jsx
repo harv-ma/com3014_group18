264fd48dc0a6965d88ab2e5b2f5c50435251a4a5
MikeM-Surrey <mm02482@surrey.ac.uk>
2022-04-26 14:00:58 +0100

    Added JobList page with basic accordion
    Added bootstrap, using the Accordion module.
    Added Router to /jobs page
    Added basic Accordion to the JobList page

--- client/src/routers/Router.jsx
+++ client/src/routers/Router.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Homepage from "../pages/Homepage/Homepage";
 import JobPage from "../pages/JobPage/JobPage";
+import JobList from "../pages/JobList/JobList";
 import NotFound from "../pages/NotFound";
 import Profile from "../pages/Profile/Profile";
 
@@ -10,6 +11,7 @@ const router = () => {
     <Routes>
       <Route path="/profile" exact element={<Profile />} />
       <Route path="/" exact element={<Homepage />} />
+      <Route path="/jobs" exact element={<JobList />} />
       <Route path="/jobs/:job_id" exact element={<JobPage />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
