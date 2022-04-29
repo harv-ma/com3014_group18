de20c2832531c4f41048b0794ff70ae63774d20f
Harvey <58261834+harv-ma@users.noreply.github.com>
2022-04-29 17:23:42 +0100

    Added mobile menu

--- client/src/context/UserContext.jsx
+++ client/src/context/UserContext.jsx
@@ -8,11 +8,13 @@ const UserContext = React.createContext(
     username: "",
     password: "",
     avatar: "",
+    isLoggedIn: false,
   },
   {
     username: "NewUser1",
     password: "",
     avatar: "https://picsum.photos/id/1069/300/300",
+    isLoggedIn: false,
   }
 );
 
