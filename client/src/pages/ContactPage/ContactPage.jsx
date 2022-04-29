43df5fd69560cf6840a9673766158e242a7f9983
Shilesh <ss02209@surrey.ac.uk>
2022-04-29 16:39:04 +0100

    contact page fix

--- client/src/pages/ContactPage/ContactPage.jsx
+++ client/src/pages/ContactPage/ContactPage.jsx
@@ -7,7 +7,7 @@ const ContactPage = () => {
       <main id ="contactpage">
           <Reeds />
           <p>Find out about us or contact us</p>
-            <h1 className = "About us"></h1>
+            <h1 className = "About Us"></h1>
             <h2 className = "Need help? Contact us?"></h2>
       </main>  
     
