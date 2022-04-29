05b4466db3954794f0f2d7b6253fb5b17986e00b
Shilesh <ss02209@surrey.ac.uk>
2022-04-29 16:54:44 +0100

    contact pange changes

--- client/src/pages/ContactPage/ContactPage.jsx
+++ client/src/pages/ContactPage/ContactPage.jsx
@@ -6,7 +6,7 @@ const ContactPage = () => {
     return (
       <main id ="contactpage">
           <Reeds />
-          <p>Find out about us or contact us</p>
+          <p>Find out about us or contact us.</p>
             <h1 className = "About Us"></h1>
             <h2 className = "Need help? Contact Us."></h2>
       </main>  
