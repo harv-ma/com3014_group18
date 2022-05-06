2f33f33ed2ce698ba492644993d5c0f035968bff
Patrick Oramah <patrickoramah98@gmail.com>
2022-05-06 22:56:27 +0100

    dockerfile update

--- backend/job-service/src/test/java/com/indireed/jobservice/JobServiceApplicationTests.java
+++ backend/job-service/src/test/java/com/indireed/jobservice/JobServiceApplicationTests.java
@@ -1,9 +1,10 @@
 package com.indireed.jobservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@DataJpaTest
 class JobServiceApplicationTests {
 
 	@Test
