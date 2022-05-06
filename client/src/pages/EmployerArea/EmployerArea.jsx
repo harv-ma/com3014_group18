7b94e2d4fa132e702907e08f77ccd8657fefeb5d
MikeM-Surrey <mm02482@surrey.ac.uk>
2022-05-06 12:42:22 +0100

    Fixed formatting in Employer Area
    indenting was going crazy, other small QOL fixes

--- client/src/pages/EmployerArea/EmployerArea.jsx
+++ client/src/pages/EmployerArea/EmployerArea.jsx
@@ -14,7 +14,7 @@ const EmployerArea = () => {
         <div className="subtitle">Phone Number: (Phone Number)</div>
         <div className="subtitle">Website: (Website)</div>
       </div>
-      <button>Update Details</button>
+      <div><button>Update Details</button></div>
     </div>
     <div className="title">Company Description</div>
     <div>
@@ -32,139 +32,139 @@ const EmployerArea = () => {
     <p></p>
     <div className="title">Jobs Open</div>
     <div className="JobsAccordion">
-                <Accordion defaultActiveKey="0" alwaysOpen>
-                    <Accordion.Item eventKey="0">
-                        <Accordion.Header>
-                            <div id="container">
-                                <div id="left">Job Description #1</div>
-                                <div id="centre">Job Location</div>
-                                <div id="right">Applications: #</div>
-                            </div>
-                        </Accordion.Header>
-                        <Accordion.Body>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                            commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                            est laborum.
-                            <p></p>
-                            <button>See more</button>
-                            <p></p>
-                            <div className="ApplicantsAccordion">
-                                <Accordion defaultActiveKey="0">
-                                    <Accordion.Item eventKey="0">
-                                        <Accordion.Header>
-                                            <div id="container">
-                                                <div id="left">Applicant 1 Name</div>
-                                                <div id="right">Job Title</div>
-                                            </div>
-                                        </Accordion.Header>
-                                        <Accordion.Body>
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                                            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                                            commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                                            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                                            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                                            est laborum.
-                                            <p></p>
-                                            <button>See application</button>
-                                        </Accordion.Body>
-                                    </Accordion.Item>
-                                    <Accordion.Item eventKey="1">
-                                        <Accordion.Header>
-                                            <div id="container">
-                                                <div id="left">Applicant 2 Name</div>
-                                                <div id="right">Job Title</div>
-                                            </div>
-                                        </Accordion.Header>
-                                        <Accordion.Body>
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                                            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                                            commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                                            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                                            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                                            est laborum.
-                                            <p></p>
-                                            <button>See application</button>
-                                        </Accordion.Body>
-                                    </Accordion.Item>
-                                </Accordion>
-                            </div>
+        <Accordion defaultActiveKey="0" alwaysOpen>
+            <Accordion.Item eventKey="0">
+                <Accordion.Header>
+                    <div id="container">
+                        <div id="left">Job Description #1</div>
+                        <div id="centre">Job Location</div>
+                        <div id="right">Applications: #</div>
+                    </div>
+                </Accordion.Header>
+                <Accordion.Body>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                    commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                    cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                    est laborum.
+                    <p></p>
+                    <button>Edit Job</button>
+                    <p></p>
+                    <div className="ApplicantsAccordion">
+                        <Accordion defaultActiveKey="0">
+                            <Accordion.Item eventKey="0">
+                                <Accordion.Header>
+                                    <div id="container">
+                                        <div id="left">Applicant 1 Name</div>
+                                        <div id="right">Job Title</div>
+                                    </div>
+                                </Accordion.Header>
+                                <Accordion.Body>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                                    commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                                    cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                                    est laborum.
+                                    <p></p>
+                                    <button>See application</button>
+                                </Accordion.Body>
+                            </Accordion.Item>
+                            <Accordion.Item eventKey="1">
+                                <Accordion.Header>
+                                    <div id="container">
+                                        <div id="left">Applicant 2 Name</div>
+                                        <div id="right">Job Title</div>
+                                    </div>
+                                </Accordion.Header>
+                                <Accordion.Body>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                                    commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                                    cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                                    est laborum.
+                                    <p></p>
+                                    <button>See application</button>
+                                </Accordion.Body>
+                            </Accordion.Item>
+                        </Accordion>
+                    </div>
 
-                        </Accordion.Body>
-                    </Accordion.Item>
-                    <Accordion.Item eventKey="1">
-                        <Accordion.Header>
-                            <div id="container">
-                                <div id="left">Job Description #2</div>
-                                <div id="centre">Job Location</div>
-                                <div id="right">Applications: #</div>
-                            </div>
-                        </Accordion.Header>
-                        <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                        est laborum.
+                </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="1">
+                <Accordion.Header>
+                    <div id="container">
+                        <div id="left">Job Description #2</div>
+                        <div id="centre">Job Location</div>
+                        <div id="right">Applications: #</div>
+                    </div>
+                </Accordion.Header>
+                <Accordion.Body>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                est laborum.
 
-                        <p></p>
-                        <button>See more</button>
-                        <p></p>
+                <p></p>
+                <button>Edit Job</button>
+                <p></p>
 
-                        <div className="ApplicantsAccordion">
-                                <Accordion defaultActiveKey="0">
-                                    <Accordion.Item eventKey="0">
-                                        <Accordion.Header>
-                                            <div id="container">
-                                                <div id="left">Applicant 1 Name</div>
-                                                <div id="right">Job Title</div>
-                                            </div>
-                                        </Accordion.Header>
-                                        <Accordion.Body>
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                                            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                                            commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                                            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                                            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                                            est laborum.
-                                            <p></p>
-                                            <button>See application</button>
-                                        </Accordion.Body>
-                                    </Accordion.Item>
-                                    <Accordion.Item eventKey="1">
-                                        <Accordion.Header>
-                                            <div id="container">
-                                                <div id="left">Applicant 2 Name</div>
-                                                <div id="right">Job Title</div>
-                                            </div>
-                                        </Accordion.Header>
-                                        <Accordion.Body>
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                                            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                                            commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                                            velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                                            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                                            est laborum.
-                                            <p></p>
-                                            <button>See application</button>
-                                        </Accordion.Body>
-                                    </Accordion.Item>
-                                </Accordion>
-                            </div>
+                <div className="ApplicantsAccordion">
+                        <Accordion defaultActiveKey="0">
+                            <Accordion.Item eventKey="0">
+                                <Accordion.Header>
+                                    <div id="container">
+                                        <div id="left">Applicant 1 Name</div>
+                                        <div id="right">Job Title</div>
+                                    </div>
+                                </Accordion.Header>
+                                <Accordion.Body>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                                    commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                                    cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                                    est laborum.
+                                    <p></p>
+                                    <button>See application</button>
+                                </Accordion.Body>
+                            </Accordion.Item>
+                            <Accordion.Item eventKey="1">
+                                <Accordion.Header>
+                                    <div id="container">
+                                        <div id="left">Applicant 2 Name</div>
+                                        <div id="right">Job Title</div>
+                                    </div>
+                                </Accordion.Header>
+                                <Accordion.Body>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                                    commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                                    cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                                    est laborum.
+                                    <p></p>
+                                    <button>See application</button>
+                                </Accordion.Body>
+                            </Accordion.Item>
+                        </Accordion>
+                    </div>
 
-                        </Accordion.Body>
-                    </Accordion.Item>
-                </Accordion>
-            </div>
+                </Accordion.Body>
+            </Accordion.Item>
+        </Accordion>
+    </div>
   </main>);
 };
 
