6e34207147bcf813de57c602611c39aef306fc63
Patrick Oramah <patrickoramah98@gmail.com>
2022-05-07 16:37:14 +0100

    Renamed keycloak admin client config

--- backend/user-service/src/main/java/com/indireed/userservice/config/KeycloakAdminClientConfig.java
+++ backend/user-service/src/main/java/com/indireed/userservice/config/KeycloakAdminClientConfig.java
@@ -3,7 +3,7 @@ package com.indireed.userservice.config;
 
 import org.keycloak.admin.client.Keycloak;
 
-public class KeycloakClientConfig {
+public class KeycloakAdminClientConfig {
 
     public static Keycloak getKeyCloak() {
         return Keycloak.getInstance("http://keycloak:8080",
