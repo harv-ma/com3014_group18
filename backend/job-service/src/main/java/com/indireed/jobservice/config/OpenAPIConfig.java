95d6ab2245b2ad5f2affebb8c89432e34479c9a7
Patrick Oramah <patrickoramah98@gmail.com>
2022-05-08 03:04:16 +0100

    Keycloack config

---
+++ backend/job-service/src/main/java/com/indireed/jobservice/config/OpenAPIConfig.java
@@ -0,0 +1,19 @@
+package com.indireed.jobservice.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info().title("User Service API").description(
+                        "API documentation for user service"));
+    }
+}
